224c1d87990ad05c24c16fb60dc87b7c4d6ad6d3
Russell Steffen <rsteffen@gmail.com>
2019-03-30 12:58:58 -0700

    move mpi/compiler crufting to cmake

--- build.sh
+++ build.sh
@@ -1,17 +1,20 @@
 #!/bin/bash -x
 
-if [ -d ./build ] ; then
+if [ -d ./build ]; then
 	rm -rf ./build
 fi
 
 mkdir build
 
-export OMPI_CC=gcc-8
-export OMPI_CXX=c++-8
-export CC=$(which mpicc)
-export CXX=$(which mpicxx)
 CMAKE_OPTIONS="-DCMAKE_BUILD_TYPE=Debug"
+#CMAKE_OPTIONS="-DCMAKE_BUILD_TYPE=Release"
 
 cmake ${CMAKE_OPTIONS} . -B build
-( cd build ; cmake ${CMAKE_OPTIONS} .. )
-( cd build ; cmake --build . )
+(
+	cd build
+	cmake ${CMAKE_OPTIONS} ..
+)
+(
+	cd build
+	cmake --build .
+)
