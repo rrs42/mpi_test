2e529aea3218d46b65e884692f17274cd1ffd2c2
Russell Steffen <rsteffen@gmail.com>
2019-03-30 13:17:19 -0700

    add release/debug clean options

--- build.sh
+++ build.sh
@@ -1,12 +1,34 @@
 #!/bin/bash -x
 
+clean_only=0
+build_type="Release"
+
+while getopts "cdr" OPTION; do
+	case "$OPTION" in
+	c)
+		echo "Cleanup..."
+		clean_only=1
+		;;
+	d)
+		build_type="Debug"
+		;;
+	r)
+		build_type="Release"
+		;;
+	esac
+done
+
 if [ -d ./build ]; then
 	rm -rf ./build
 fi
 
+if [ $clean_only == 1 ]; then
+	exit
+fi
+
 mkdir build
 
-CMAKE_OPTIONS="-DCMAKE_BUILD_TYPE=Debug"
+CMAKE_OPTIONS="-DCMAKE_BUILD_TYPE=${build_type}"
 #CMAKE_OPTIONS="-DCMAKE_BUILD_TYPE=Release"
 
 cmake ${CMAKE_OPTIONS} . -B build
